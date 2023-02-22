1c6cf12823c0279fc00563b84e375a993482d71b
Gianpaolo Macario <gmacario@gmail.com>
2023-02-22 12:30:39 +0100

    Add some debug messages
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
+console.log("DEBUG: Starting server on localhost:8080");
+
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello World!\n\n');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
